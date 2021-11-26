f9e09dc06a53465a6c708ee6e61539be24f1d2f6
mmdbalkhi <ahmdparsh129@gmail.com>
2021-11-26 17:00:22 +0330

    rename module name and add v1 to funcs

--- flaskauthapi/security.py
+++ flaskauthapi/security.py
@@ -2,20 +2,21 @@ import hashlib
 from typing import AnyStr, Dict
 
 from config import secret_key
+from sqlite import read_sql_v1
 
 
-def hash_data(data: Dict, salt=secret_key) -> str:
+def hash_data_v1(data: Dict, salt=secret_key) -> str:
     data["salt"] = salt
     hashcode = hashlib.sha3_512(str(data)[::-1].encode("utf8"))
     return hashcode.hexdigest()
 
 
-def hash_username(username: AnyStr) -> str:
+def hash_username_v1(username: AnyStr) -> str:
     hashcode = hashlib.sha256(username.encode("utf8"))
     return hashcode.hexdigest()
 
 
-def password_secure(username: AnyStr, password: AnyStr) -> bool:
+def password_secure_v1(username: AnyStr, password: AnyStr) -> bool:
     if username == password:
         return False
     elif password.isdigit() or len(password) < 8:
@@ -24,14 +25,14 @@ def password_secure(username: AnyStr, password: AnyStr) -> bool:
     return True
 
 
-def check_corrected(username: AnyStr, password: AnyStr) -> bool:
+def check_corrected_v1(username: AnyStr, password: AnyStr) -> bool:
     data = {"username": username, "password": password}
-    data_hash = hash_data(data)
+    data_hash = hash_data_v1(data)
 
-    if not read_sql(username):
+    if not read_sql_v1(username):
         return False
 
-    if read_sql(username)[0] == data_hash:
+    if read_sql_v1(username)[0] == data_hash:
         return True
 
     return
